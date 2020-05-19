4ef6401fd4c65fa448daa2d18f0c3b93a84dc4e4
Jordan <jalieb0812@gmail.com>
2020-05-19 13:40:01 -0400

    finished making models v.1 5-19-2020

--- orders/models.py
+++ orders/models.py
@@ -1,100 +1,160 @@
 from django.db import models
 
 # Create your models here.
-class Toppings(models.Model):
-    topping = models.CharField(max_length=64)
+
+class Menu_Item(models.Model):
+
+    MENU_CATEGORIES = (
+    ('Pizza', 'Pizza'),
+    ('Pasta', 'Pasta'),
+    ('Subs', 'Subs'),
+    ('Salad', 'Salad'),
+    ('Dinner_Platter', 'Dinner_Platter'),
+    ('Topping', 'Topping'),
+    ('Extra', 'Extra'),
+    ('Dessert', 'Dessert'),
+    ('Pastry', 'Pastry'),
+    ('Main', 'Main'),
+    ('Appetizer', 'Appetizer'),
+    ('Side', 'Side'),
+    ('Miscellaneous', 'Miscellaneous')
+    )
+
+    SIZE_CATEGORIES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra_Large')
+    )
+
+    category = models.CharField(max_length=1, null=True, blank=True, choices=MENU_CATEGORIES,
+                                help_text='Enter the category of the menu item')
+
+    name = models.CharField(max_length=128, help_text='Enter name of the menu item')
+
+    price = price =  models.DecimalField(max_digits=4, null=True, blank=True, decimal_places=2)
+
+    sizes = models.CharField(max_length=4, null=True, blank=True, choices=SIZE_CATEGORIES,
+                                help_text='Enter the allowable sizes of the menu item')
+
 
     def __str__(self):
-        return f"{self.topping}"
+        return f" Category:{self.category} - Name:{self.name} - Sizes:{self.size} - Price: {self.price}"
+
+class Toppings(models.Model):
+    name = models.CharField(max_length=36)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Extras(models.Model):
+    name = models.CharField(max_length=64, help_text='Enter name of the extra')
+    price = models.DecimalField(max_digits=4,decimal_places=2, default=0, help_text='Enter price of the extra')
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
 
 class Pizza(models.Model):
 
     name = "Regular Pizza"
 
-    size = models.CharField(max_length=10, help_text='Enter Size of Pizza: Small or Large') #maybe extra Large
+    SIZE_CATEGORIES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra_Large')
+    )
 
+
+    size = models.CharField(max_length=1, choices=SIZE_CATEGORIES,
+                            help_text='Enter pizza size')
 
     price =  models.DecimalField(max_digits=4,decimal_places=2)
     #topings = model.
 
-    topping_price = models.DecimalField(max_digits=2,decimal_places=2)
-    num_toppings = models.DecimalField(max_digits=10, decimal_places=0, default=0)
+    num_toppings = models.IntegerField(default=0)
 
-    """
-    if num_toppings == 1:
-        topping_price = 12.70
-
-    if num_toppings == 2:
-        topping_price = 13.70
-
-    if num_toppings == 3:
-        topping_price = 16.20
-    """
-    reg_toppings = models.ForeignKey(Toppings, on_delete=models.CASCADE, related_name="Reg_extras")
+    toppings = models.ManyToManyField(Toppings, verbose_name="Toppings",  related_name="reg_toppings")
 
     #total_price= price + topping_price
 
     def __str__(self):
         return f" Item:{self.name} Size:{self.size} Number of toppings: {self.num_toppings}; \
-        toppings {self.reg_toppings} cost$: {self.price} total_price = {self.total_price}"
+        toppings {self.toppings} price$: {self.price}"
 
 
 class Sicilian_Pizza(models.Model):
 
     name = "Sicilian Pizza"
 
-    size = models.CharField(max_length=10) #maybe extra Large
+    SIZE_CATEGORIES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra_Large')
+    )
 
-    price = models.DecimalField(max_digits=4,decimal_places=2)
 
-    num_toppings = models.IntegerField(blank=True, default=0)
+    size = models.CharField(max_length=1, choices=SIZE_CATEGORIES,
+                            help_text='Enter pizza size')
 
-    sic_toppings = models.ForeignKey(Toppings, on_delete=models.CASCADE, related_name="Sic_extras")
+    price =  models.DecimalField(max_digits=4,decimal_places=2)
+    #topings = model.
+
+    num_toppings = models.IntegerField(default=0)
+
+    toppings = models.ManyToManyField(Toppings, verbose_name="Toppings",  related_name="Sic_toppings")
 
     def __str__(self):
         return f" Item{self.name} Size:{self.size}; Number of toppings {self.num_toppings}; \
-        toppings: {self.sic_toppings}; cost$: {self.price}"
+        toppings: {self.toppings}; price$: {self.price}"
 
 
 
 class Subs(models.Model):
-    size = models.CharField(max_length=10) #maybe extra Large
-    price = models.DecimalField(max_digits=4,decimal_places=2)
-    #type =
-    #extras =
+
+    name = models.CharField("Sub Type", max_length=36, help_text='Enter sub type')
+
+    SIZE_CATEGORIES = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra_Large')
+    )
+
+    size = models.CharField(max_length=1, choices=SIZE_CATEGORIES,
+                            help_text='Enter sub size')
+
+
+    price = models.DecimalField(max_digits=4,decimal_places=2, help_text='Enter sub price')
+
+    extras = models.ManyToManyField(Extras, verbose_name="Extras",  related_name="sub_extra")
 
     def __str__(self):
-        return f"{self.size}  cost$: {self.price}"
+        return f" Sub:{self.name}- Size:{self.size}  price$: {self.price}"
 
-class Sub_Types(models.Model):
-    sub_type = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.sub_type}"
-
-
-class Sub_Extras(models.Model):
-    sub_extra = models.CharField(max_length=64)
-    num_extras = models.IntegerField()
-    def __str__(self):
-        return f"{self.sub_extra} - {self.num_extras}"
 
 class Pasta(models.Model):
-    price = models.IntegerField()
+
+    name = models.CharField("Pasta Type", max_length=36, help_text='Enter pasta type')
+    price = models.DecimalField(max_digits=4,decimal_places=2, help_text='Enter pasta price')
     #type =
     def __str__(self):
-        return f"cost$: {self.price}"
+        return f" Pasta_type: {self.name} price$: {self.price}"
 
 class Salad(models.Model):
-    price = models.IntegerField()
+
+    name = models.CharField("Salad Type", max_length=36, help_text='Enter salad type')
+    price = models.DecimalField(max_digits=4,decimal_places=2, help_text='Enter salad price')
     #type =
     def __str__(self):
-        return f" cost$: {self.price}"
+        return f" Salad_type: {self.name} price$: {self.price}"
 
 class Dinner_Platter(models.Model):
 
-    size = models.CharField(max_length=10) #maybe extra Large
-    price = models.DecimalField(max_digits=4,decimal_places=2)
+    name = models.CharField("Dinner Platters", max_length=36, help_text='Enter type of Dinner Platter')
+    price = models.DecimalField(max_digits=4,decimal_places=2, help_text='Enter Diiner Platter price')
     #type =
     def __str__(self):
-        return f"{self.size}  cost$: {self.price}"
+        return f" Salad_type: {self.name} price$: {self.price}"
