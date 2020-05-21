8c50ddada7a40fcf54e5fc4db1ab7b426e8bf7b5
Jordan <jalieb0812@gmail.com>
2020-05-20 20:03:16 -0400

    started on menu

--- orders/views.py
+++ orders/views.py
@@ -13,17 +13,34 @@ def index(request):
     menu_items = Menu_Item.objects.all()
 
 
+
+
     pizza_categories = Menu_Item.objects.filter(category__contains="Pizza")
 
+    toppings = Menu_Item.objects.filter(category__contains="Topping")
 
+    extras = Menu_Item.objects.filter(category__contains="Extra")
 
+    sub_categories = Menu_Item.objects.filter(category__contains="Subs")
 
+    salad_categories = Menu_Item.objects.filter(category__contains="Salad")
+
+    dinner_platter_categories = Menu_Item.objects.filter(category__contains="Dinner_Platter")
+
+    pasta_categories = Menu_Item.objects.filter(category__contains="Pasta")
 
 
     context ={
 
+
         "menu_item": menu_items,
         "pizza_categories": pizza_categories,
+        "toppings": toppings,
+        "extras": extras,
+        "sub_categories": sub_categories,
+        "salad_categories": salad_categories,
+        "dinner_platter_categories": dinner_platter_categories,
+        "pasta_categories": pasta_categories,
 
         "Pizza": Pizza.objects.all(),
         "pizza_toppings": Pizza.toppings,
