59c042159789def768d7ba8616d80073260c6af2
Jordan <jalieb0812@gmail.com>
2020-06-01 12:46:53 -0400

    fixed some styling

--- orders/views.py
+++ orders/views.py
@@ -10,7 +10,7 @@ from django.utils.datastructures import MultiValueDictKeyError
 #from accounts.models import Profile
 
 from .models import (Pizza, Topping, Menu_Item, Pasta, Profile,
-Subs, Salad, Dinner_Platter, Extras, Order, OrderItem, Transaction, User, )
+Subs, Salad, Dinner_Platter, Extras, Order, OrderItem,  User, )
 
 import datetime
 
@@ -78,9 +78,11 @@ def index(request):
     pasta_categories = Menu_Item.objects.filter(category__contains="Pasta")
 
 
+
     context ={
 
         'current_order_products': current_order_products,
+
 
         "user": request.user,
 
@@ -139,7 +141,7 @@ def add_to_cart(request, **kwargs):
     #messages.info(request, "item added to cart")
 
 
-    return HttpResponseRedirect(reverse('orders:index'))
+    return HttpResponseRedirect(reverse('orders:index', message = "order placed!."))
 
 @login_required()
 def delete_from_cart(request, item_id):
@@ -258,6 +260,14 @@ def customize_order(request, food, **kwargs):
             num_extras += 1
 
     #use get to get the price of extras
+
+
+    sub_extra = Menu_Item.objects.get(name="Sub_Extra")
+
+    extra_price = sub_extra.price
+
+    print (f"extra price is {extra_price}")
+
 
     extras_cost = num_extras * 0.50
 
@@ -466,128 +476,6 @@ def success(request, **kwargs):
     return render(request, 'orders/purchase_success.html', context)
 
 
-
-# @login_required()
-# def delete_from_cart(request, item_id):
-#     item_to_delete = OrderItem.objects.filter(pk=item_id)
-#     if item_to_delete.exists():
-#         item_to_delete[0].delete()
-#         messages.info(request, "Item has been deleted")
-#     return redirect(reverse('shopping_cart:order_summary'))
-
-
-# @login_required()
-# def order_details(request, **kwargs):
-#     existing_order = get_user_pending_order(request)
-#     context = {
-#         'order': existing_order
-#     }
-#     return render(request, 'shopping_cart/order_summary.html', context)
-#
-# def get_user_pending_order(request):
-#     # get order for the correct user
-#     user_profile = get_object_or_404(Profile, user=request.user)
-#     order = Order.objects.filter(owner=user_profile, is_ordered=False)
-#     if order.exists():
-#         # get the only order in the list of filtered orders
-#         return order[0]
-#     return 0
-#
-# @login_required()
-# def checkout(request, **kwargs):
-#     client_token = generate_client_token()
-#     existing_order = get_user_pending_order(request)
-#     publishKey = settings.STRIPE_PUBLISHABLE_KEY
-#     if request.method == 'POST':
-#         token = request.POST.get('stripeToken', False)
-#         if token:
-#             try:
-#                 charge = stripe.Charge.create(
-#                     amount=100*existing_order.get_cart_total(),
-#                     currency='usd',
-#                     description='Example charge',
-#                     source=token,
-#                 )
-#
-#                 return redirect(reverse('shopping_cart:update_records',
-#                         kwargs={
-#                             'token': token
-#                         })
-#                     )
-#             except stripe.CardError as e:
-#                 message.info(request, "Your card has been declined.")
-#         else:
-#             result = transact({
-#                 'amount': existing_order.get_cart_total(),
-#                 'payment_method_nonce': request.POST['payment_method_nonce'],
-#                 'options': {
-#                     "submit_for_settlement": True
-#                 }
-#             })
-#
-#             if result.is_success or result.transaction:
-#                 return redirect(reverse('shopping_cart:update_records',
-#                         kwargs={
-#                             'token': result.transaction.id
-#                         })
-#                     )
-#             else:
-#                 for x in result.errors.deep_errors:
-#                     messages.info(request, x)
-#                 return redirect(reverse('shopping_cart:checkout'))
-#
-#     context = {
-#         'order': existing_order,
-#         'client_token': client_token,
-#         'STRIPE_PUBLISHABLE_KEY': publishKey
-#     }
-#
-#     return render(request, 'shopping_cart/checkout.html', context)
-#
-#
-# @login_required()
-# def update_transaction_records(request, token):
-#     # get the order being processed
-#     order_to_purchase = get_user_pending_order(request)
-#
-#     # update the placed order
-#     order_to_purchase.is_ordered=True
-#     order_to_purchase.date_ordered=datetime.datetime.now()
-#     order_to_purchase.save()
-#
-#     # get all items in the order - generates a queryset
-#     order_items = order_to_purchase.items.all()
-#
-#     # update order items
-#     order_items.update(is_ordered=True, date_ordered=datetime.datetime.now())
-#
-#     # Add products to user profile
-#     user_profile = get_object_or_404(Profile, user=request.user)
-#     # get the products from the items
-#     order_products = [item.product for item in order_items]
-#     user_profile.ebooks.add(*order_products)
-#     user_profile.save()
-#
-#
-#     # create a transaction
-#     transaction = Transaction(profile=request.user.profile,
-#                             token=token,
-#                             order_id=order_to_purchase.id,
-#                             amount=order_to_purchase.get_cart_total(),
-#                             success=True)
-#     # save the transcation (otherwise doesn't exist)
-#     transaction.save()
-#
-#
-#     # send an email to the customer
-#     # look at tutorial on how to send emails with sendgrid
-#     messages.info(request, "Thank you! Your purchase was successful!")
-# #     return redirect(reverse('accounts:my_profile'))
-#
-#
-# def success(request, **kwargs):
-#     # a view signifying the transcation was successful
-#     return render(request, 'shopping_cart/purchase_success.html', {})
 
 def register_view(request):
 
